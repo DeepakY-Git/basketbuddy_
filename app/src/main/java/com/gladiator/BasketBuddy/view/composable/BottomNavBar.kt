d608d32daee41bfedf694546f56a515cc45df613
Akshita <akshitak2103@gmail.com>
2026-03-12 16:59:55 +0530

    Bottom Nav Bar UI Update

--- app/src/main/java/com/gladiator/BasketBuddy/view/composable/BottomNavBar.kt
+++ app/src/main/java/com/gladiator/BasketBuddy/view/composable/BottomNavBar.kt
@@ -1,6 +1,7 @@
 package com.gladiator.BasketBuddy.view.composable
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Groups
 import androidx.compose.material.icons.filled.Home
@@ -8,6 +9,7 @@ import androidx.compose.material.icons.filled.Logout
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,7 +17,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
@@ -23,7 +28,9 @@ import androidx.navigation.compose.rememberNavController
 fun BasketBuddyBottomNav(navController: NavController) {
     var selectedIndex by remember { mutableStateOf(0) }
 
-    NavigationBar {
+    NavigationBar (
+        containerColor = Color(0xFFF5EBDD)
+    ){
         NavigationBarItem(
             selected = selectedIndex == 0,
             onClick = {
@@ -32,18 +39,29 @@ fun BasketBuddyBottomNav(navController: NavController) {
                     popUpTo("home") { inclusive = true } // Prevents stack buildup
                 }
             },
-            icon = { Icon(Icons.Filled.Home, contentDescription = "Home") },
-            label = { Text("Home") }
+            icon = { Icon(Icons.Filled.Home, contentDescription = "Home", modifier = Modifier.size(26.dp)) },
+            label = { Text("Home") },
+            colors = NavigationBarItemDefaults.colors(selectedIconColor = Color(0xFFB08968),
+                selectedTextColor = Color(0xFFB08968),
+                unselectedIconColor = Color.Gray,
+                unselectedTextColor = Color.Gray,
+                indicatorColor = Color(0xFFEAD8C0)
+            )
         )
-
         NavigationBarItem(
             selected = selectedIndex == 1,
             onClick = {
                 selectedIndex = 1
                 navController.navigate("collaboration")
             },
-            icon = { Icon(Icons.Filled.Groups, contentDescription = "Group") },
-            label = { Text("Group") }
+            icon = { Icon(Icons.Filled.Groups, contentDescription = "Group", modifier = Modifier.size(26.dp))
+                   },
+            label = { Text("Group") },
+            colors = NavigationBarItemDefaults.colors(selectedIconColor = Color(0xFFB08968),
+                selectedTextColor = Color(0xFFB08968),
+                unselectedIconColor = Color.Gray,
+                unselectedTextColor = Color.Gray,
+                indicatorColor = Color(0xFFEAD8C0))
         )
 
         NavigationBarItem(
@@ -52,8 +70,14 @@ fun BasketBuddyBottomNav(navController: NavController) {
                 selectedIndex = 2
                 navController.navigate("login")
             },
-            icon = { Icon(Icons.Default.Logout, contentDescription = "List") },
-            label = { Text("Logout") }
+            icon = { Icon(Icons.Default.Logout, contentDescription = "Logout") },
+            label = { Text("Logout") },
+            colors = NavigationBarItemDefaults.colors(selectedIconColor = Color(0xFFB08968),
+                selectedTextColor = Color(0xFFB08968),
+                unselectedIconColor = Color.Gray,
+                unselectedTextColor = Color.Gray,
+                indicatorColor = Color(0xFFEAD8C0)
+            )
         )
     }
 }
